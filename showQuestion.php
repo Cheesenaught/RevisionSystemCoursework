1d2268af5e4f7f9cd50f1657930ea2771ac7673a
Cheesenaught <ethandan2007@gmail.com>
2025-02-28 20:52:00 +0000

    change

---
+++ showQuestion.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <title>Settings</title>
+    <?php include_once("connection.php"); ?>
+    </head>
+<body>
+<?php include_once("navbar.php");
+if(isset ($_SESSION["UserID"])){
+  $stmt = $conn->prepare("SELECT * FROM TblUsers WHERE UserID = ".$_SESSION["UserID"]);
+  $stmt->execute(); 
+  while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+      echo("Forename: " . $row["Question"] . "<br>");
+      echo("Surname: " . $row["Surname"] . "<br>");
+  }
+}
+?>
+</body>
+</html>
